7e2d3a375becde5a1de3af566d1ae9a6c3fda16d
ywisax <25803471@qq.com>
2015-08-29 15:44:01 +0800

    init

---
+++ bootstrap-dev.php
@@ -0,0 +1,6 @@
+<?php
+
+if (is_file(__DIR__ . '/vendor/autoload.php'))
+{
+    require_once __DIR__ . '/vendor/autoload.php';
+}
---
+++ src/Exception/ViewException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tourze\View\Exception;
+
+use tourze\Base\Exception\BaseException;
+
+/**
+ * 视图异常处理类
+ *
+ * @package tourze\View\Exception
+ */
+class ViewException extends BaseException
+{
+}

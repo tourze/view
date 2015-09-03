e7747105851306065ffd424528ce410f38da6360
ywisax <25803471@qq.com>
2015-09-04 02:51:52 +0800

    更新依赖

--- src/View.php
+++ src/View.php
@@ -3,6 +3,7 @@
 namespace tourze\View;
 
 use Exception;
+use tourze\Base\Base as TourzeBase;
 
 /**
  * 原生php的视图处理类
@@ -24,6 +25,10 @@ class View extends Base implements ViewInterface
      */
     protected function capture($viewFilename, array $viewData)
     {
+        TourzeBase::getLog()->debug(__METHOD__ . ' capture view output', [
+            'file' => $viewFilename,
+        ]);
+
         // 导入变量
         extract($viewData, EXTR_SKIP);
         if ( ! empty(View::$_globalData))
